6e95f5efb4115b0cf5fe12a2984f75244b5c282f
stonedjjh <danjim82@gmail.com>
2025-12-12 06:22:21 -0400

    Feature: Creacion de una pagina de Heroes, aplicando los requerimientos de la tarea asignada en el curso

---
+++ DevTalles/Angular/02-bases/src/app/pages/hero/hero-page.ts
@@ -0,0 +1,48 @@
+import { Component, signal, computed } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
+
+//Se usa el decorator Component para definir un componente en Angular
+@Component({
+  selector: 'hero-page',
+  templateUrl: './hero-page.html',
+  imports: [UpperCasePipe],
+})
+//Definimos la clase del componente y su lógica
+export class HeroPage {
+  name = signal<string>('');
+  age = signal<number>(0);
+
+  constructor() {
+    this.name.set('Ironman');
+    this.age.set(45);
+  }
+
+  nameUpper = computed(() => this.name().toUpperCase());
+
+  heroDescription = computed(() => {
+    const description = `Super Heroe: ${this.name()} - Edad: ${this.age()}`;
+    return description;
+  });
+
+  //   getHeroDescription() {
+  //     return `Super Heroe: ${this.name().toUpperCase()} - Edad: ${this.age()}`;
+  //   }
+
+  updateAge(newAge: number) {
+    this.age.set(newAge);
+  }
+
+  changeHero() {
+    this.name.set('Spiderman');
+    this.age.set(22);
+  }
+
+  changeAge() {
+    this.age.set(60);
+  }
+
+  resetForm() {
+    this.name.set('Ironman');
+    this.age.set(45);
+  }
+}
